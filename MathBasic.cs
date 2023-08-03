0aefb58a98498ba7c0cd28d7f084d2805247bf9a
GOLEMWORKS <korotkikh.vladimir.isr21.1@gmail.com>
2023-08-03 11:57:35 +0200

    Добавьте файлы проекта.

---
+++ MathBasic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_sofinans_calc
+{
+    public static class MathBasic
+    {
+        public static void Init()
+        {
+            Console.WriteLine("Math basic init success");
+        }  
+
+        public static string Calculate(double grantAmount, double teamHelp)
+        {
+            double res = 0;
+
+            res = (grantAmount + teamHelp) * 30 / 100;
+
+            string value = $"Объём софинансирования не более {res} рубля(ей )";
+            return value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace test_sofinans_calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prog = true;
+
+            Console.WriteLine("Володя мегахорош представляет:");
+            while (prog == true)
+            {
+                Console.WriteLine("Введите объём гранта:");
+                double grantAmount = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите объём вложений:");
+                double helpAmount = double.Parse(Console.ReadLine());
+                Console.WriteLine(MathBasic.Calculate(grantAmount, helpAmount));
+                Console.WriteLine("Введите q для выхода из программы, \nЛибо Enter для введения новых данных для рассчёта.");
+                string res = Console.ReadLine();
+
+                if (res == "q")
+                {
+                    prog = false;
+                }
+                else
+                {
+                    Console.WriteLine("Я тебя не понял, давай по новой:");
+                }
+            }
+            
+        }
+    }
+}
